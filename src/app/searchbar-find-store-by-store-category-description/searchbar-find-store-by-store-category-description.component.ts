ef30ca0ac3958a73d3399ca0cd44c1a192f2e3f5
Petros Dhespollari <info@peterdsp.dev>
2022-09-15 12:07:33 +0300

    Update component for searchbar finding store by store category description

---
+++ src/app/searchbar-find-store-by-store-category-description/searchbar-find-store-by-store-category-description.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-searchbar-find-store-by-store-category-description',
+  templateUrl: './searchbar-find-store-by-store-category-description.component.html',
+  styleUrls: ['./searchbar-find-store-by-store-category-description.component.scss']
+})
+export class SearchbarFindStoreByStoreCategoryDescriptionComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
