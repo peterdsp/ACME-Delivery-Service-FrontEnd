21b4be98664a5622da40b2000524cb1d34112eae
Petros Dhespollari <info@peterdsp.dev>
2022-09-15 13:20:45 +0300

    Create button for basket

---
+++ src/app/button-basket/button-basket.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-button-basket',
+  templateUrl: './button-basket.component.html',
+  styleUrls: ['./button-basket.component.scss']
+})
+export class ButtonBasketComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
