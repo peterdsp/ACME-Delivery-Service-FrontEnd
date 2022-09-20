ff5c2bf9fb2c85468c6b26fa1fb683e512e3f2cb
Petros Dhespollari <info@peterdsp.dev>
2022-09-20 18:01:56 +0300

    Update page-account component

--- src/app/page-account/page-account.component.ts
+++ src/app/page-account/page-account.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { AccountService } from './../services/account.service';
 
 @Component({
   selector: 'app-page-account',
@@ -6,10 +8,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./page-account.component.scss']
 })
 export class PageAccountComponent implements OnInit {
+  response: any;
+  get=false;
+  message='';
 
-  constructor() { }
+  constructor(private route:ActivatedRoute, private accountService:AccountService) { }
 
   ngOnInit(): void {
+    this.accountService.getAccountById(1).subscribe({
+      next: data => {
+        this.response = data;
+        this.get = true;
+      },
+      error: error => this.message = error,
+      complete: () => this.message = "Request Completed..."
+    });
   }
 
-}
+
+  }
